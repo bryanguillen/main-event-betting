b56f7fe0415310ae41e7c3c9a2bfa8489dd2386a
bryg217 <bryan.guillen217@gmail.com>
2021-07-29 16:22:34 -0400

    feat: add constructor and structs

--- contracts/MainEventBetting.sol
+++ contracts/MainEventBetting.sol
@@ -6,4 +6,41 @@ contract MainEventBetting {
    ******************************/
 
   string public name = "MainEventBetting";
+  address public owner;
+
+  /******************************
+   * Structs
+   ******************************/
+
+  /**
+   * Struct that represents an event -- a fight
+   */
+  struct Event {
+    uint date;
+    Fighter fighter1;
+    Fighter fighter2;
+    uint id;
+    uint winner;
+  }
+
+  /**
+   * Struct representing a fighter
+   */
+  struct Fighter {
+    string name;
+    int odds;
+    uint id;
+  }
+
+  /******************************
+   * Constructor
+   ******************************/
+
+  /**
+   * Used for setting initial sender as owner; there
+   * should be an easier way to do this
+   */
+  constructor () public {
+    owner = msg.sender;
+  }
 }
